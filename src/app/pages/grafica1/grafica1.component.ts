dd34f8d539d2e175c52cbe87fad768d60a2f7b7a
chimewe <afernandezsan@icloud.com>
2025-12-12 12:12:35 +0100

    fin seccion 3 - diseño

---
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica1',
+  standalone: false,
+  templateUrl: './grafica1.component.html',
+  styles: ``
+})
+export class Grafica1Component {
+
+}
---
+++ src/app/pages/pages/pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  standalone: false,
+  templateUrl: './pages.component.html',
+  styles: ``
+})
+export class PagesComponent {
+
+}
